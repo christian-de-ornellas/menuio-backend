352175054735ecc5038124a7e25dfe4c362ef3e3
Christian Possidonio <=>
2025-03-25 16:07:39 -0300

    refactor: password não será exibido na lista de users

--- src/models/User.ts
+++ src/models/User.ts
@@ -13,7 +13,7 @@ const UserSchema = new Schema<UserInterface>(
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true, select: false }
   },
   { timestamps: true }
 );
