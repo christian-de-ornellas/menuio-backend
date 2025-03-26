1ea5f4b65ae540c0df4a3d676e5cc3ec2f030092
Christian Possidonio <=>
2025-03-26 12:44:24 -0300

    refactor:  criação, remoção e lista de itens do cardápio funcionando corretamente

--- src/controllers/menuController.ts
+++ src/controllers/menuController.ts
@@ -1,55 +1,32 @@
 import {Request, Response} from "express";
 import Menu from "../models/Menu";
 import {ISendResponse} from "../types/sendResponse";
+import fs from "fs";
+import path from "path";
 
 export const index = async (req: Request, res: Response) => {
-    const page = parseInt(req.query.page as string) || 1; // Página padrão é 1
-    const pageSize = parseInt(req.query.pageSize as string) || 10; // Tamanho padrão é 10
+    const page = parseInt(req.query.page as string) || 1;
+    const pageSize = parseInt(req.query.pageSize as string) || 10;
 
     try {
         const items = await Menu.find()
-            .skip((page - 1) * pageSize) // Pular resultados da página anterior
-            .limit(pageSize); // Limitar o número de resultados por página
+            .skip((page - 1) * pageSize)
+            .limit(pageSize);
 
-        const totalItems = await Menu.countDocuments(); // Contar o número total de usuários
+        const totalItems = await Menu.countDocuments();
 
         const send: ISendResponse = {
             message: "Lista de itens do cardápio",
             items,
             structure: {
                 fields: [
-                    {label: "Primeiro", code: "firstName", isVisible: true},
-                    {label: "Sobrenome", code: "lastName", isVisible: true},
-                    {label: "E-mail", code: "email", isVisible: true},
+                    {label: "Título", code: "title", isVisible: true},
+                    {label: "Descrição", code: "description", isVisible: true},
+                    {label: "Imagem", code: "image", isVisible: true},
                 ],
-                filters: [
-                    {
-                        label: "Nome",
-                        field: "firstName",
-                        operator: [
-                            {label: "Igual", value: "eq"},
-                            {label: "Contém", value: "regex"},
-                        ],
-                    },
-                    {
-                        label: "Sobrenome",
-                        field: "lastName",
-                        operator: [
-                            {label: "Igual", value: "eq"},
-                            {label: "Contém", value: "regex"},
-                        ],
-                    },
-                    {
-                        label: "Email",
-                        field: "email",
-                        operator: [
-                            {label: "Igual", value: "eq"},
-                            {label: "Contém", value: "regex"},
-                        ],
-                    },
-                ],
+
             },
-            toolbar: {header: "Usuários"},
+            toolbar: {header: "Cardápio"},
             page,
             pageSize,
             totalItems,
@@ -63,7 +40,7 @@ export const index = async (req: Request, res: Response) => {
 
 export const store = async (req: Request, res: Response) => {
     try {
-        const { title, description,userId } = req.body;
+        const {title, description, userId} = req.body;
         const imageUrl = req.file ? `/uploads/${req.file.filename}` : null;
 
         const menu = new Menu({
@@ -75,19 +52,30 @@ export const store = async (req: Request, res: Response) => {
 
         await menu.save();
 
-        res.status(201).json({message:"Item cadastrado com sucesso!"});
+        res.status(201).json({message: "Item cadastrado com sucesso!"});
     } catch (error: any) {
-        res.status(400).json({ message: error.message });
+        res.status(400).json({message: error.message});
     }
 };
 
 export const remove = async (req: Request, res: Response) => {
     try {
-        const user = await Menu.findByIdAndDelete(req.params.id);
-        if (!user) {
-            return res.status(404).json({message: "Usuário não encontrado"});
+        const menu = await Menu.findById(req.params.id);
+        if (!menu) {
+            return res.status(404).json({message: "Menu não encontrado"});
         }
-        res.json({message: "Usuário removido com sucesso"});
+
+        if (menu?.image) {
+            const imagePath = path?.join(__dirname, "../../", menu?.image);
+
+            if (fs?.existsSync(imagePath)) {
+                fs?.unlinkSync(imagePath);
+            }
+        }
+
+        await Menu.findByIdAndDelete(req.params.id);
+        res.json({message: "Menu removido com sucesso"});
+
     } catch (error: any) {
         res.status(400).json({message: error.message});
     }
