116b958eac75f6d716889a7f0747a7bd7e13821f
Christian Possidonio <=>
2025-03-26 12:40:27 -0300

    refactor: simplificando a implementação do upload de imagens

--- src/libs/multer/multerConfig.ts
+++ src/libs/multer/multerConfig.ts
@@ -1,28 +1,15 @@
 import multer from "multer";
 
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "uploads/");
     },
     filename: function (req, file, cb) {
-        cb(null, `${Date.now()}-${file.originalname}`);
-    }
+        cb(null, Date.now() + "-" + file.originalname);
+    },
 });
 
-
-const fileFilter = (req: any, file: Express.Multer.File, cb: any) => {
-    const allowedMimeTypes = ["image/jpeg", "image/png", "image/jpg"];
-    if (allowedMimeTypes.includes(file.mimetype)) {
-        cb(null, true);
-    } else {
-        cb(new Error("Apenas imagens (JPG, PNG) são permitidas!"), false);
-    }
-};
-
-const upload = multer({
-    storage: storage,
-    fileFilter: fileFilter,
-    limits: { fileSize: 5 * 1024 * 1024 }
-});
+const upload = multer({ storage });
 
 export default upload;
